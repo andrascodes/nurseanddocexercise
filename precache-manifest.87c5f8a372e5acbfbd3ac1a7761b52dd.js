bbca7558b42990e5f397cc9b9e21f9d2f768ac4a
Andrew Szucs <andrew0szucs@gmail.com>
2019-01-29 15:12:26 +0100

    Updates

---
+++ precache-manifest.87c5f8a372e5acbfbd3ac1a7761b52dd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "296e3c20af3f6821ae48",
+    "url": "/nurseanddocexercise/static/js/runtime~main.296e3c20.js"
+  },
+  {
+    "revision": "b9c00e1600966f2dd574",
+    "url": "/nurseanddocexercise/static/js/main.b9c00e16.chunk.js"
+  },
+  {
+    "revision": "a3972e4c23bcd8c52422",
+    "url": "/nurseanddocexercise/static/js/1.a3972e4c.chunk.js"
+  },
+  {
+    "revision": "f05d4e14e1a2693112616300a499ab8c",
+    "url": "/nurseanddocexercise/index.html"
+  }
+];
